d49da0e85080b8e60c4d29e2f9183265238e4178
Alexander Berndt <alberndt@kth.se>
2020-09-22 15:56:07 +0200

    Part I

---
+++ main.m
@@ -0,0 +1,126 @@
+%% FEM3200 - Optimal Filtering 
+
+% Project: Kalman Filter design for GPS receiver
+%
+% Authors: Alexander Berndt, Rebecka Winqvist
+%
+% Date: 22 September 2020
+
+%% File Structure
+
+% The following files are here (./main.m):
+
+%   ./data/GPSdata.mat
+%   ./NonLinearLeastSquares.m
+
+% This script also uses Matlab2Tikz 
+%   - make sure matlab2tikz/src is in your MATLAB path
+
+%% Initialisation
+
+clc
+clear
+
+load("data/GPSdata.mat");
+currentFolder = pwd;
+
+%% Part I
+
+% estimate car trajectory using NL least-squares algorithm
+est = NonLinearLeastSquares(gps_data, 2); %ref_data_struct.s2r);
+
+x_h = est.x_h;
+P = est.P;
+
+% obtain coordinate values 
+x = x_h(1,:);
+y = x_h(2,:);
+z = x_h(3,:);
+a = x_h(4,:);
+
+% plot x-y 2D
+gcf1 = figure(1);
+plot(x,y);
+grid on
+xlabel("x [m]");
+ylabel("y [m]");
+saveas(gcf1, "./plots/nl_estimator_traj_2D.svg");
+tikzfilename = strcat(currentFolder,'/tikzfiles/nl_estimator_traj_2D.tex');
+cleanfigure; 
+matlab2tikz('filename',tikzfilename);
+
+% plot x-y-z 3D
+gcf2 = figure(2);
+plot3(x,y,z);
+grid on
+xlabel("x [m]");
+ylabel("y [m]");
+zlabel("z [m]");
+zlim([-100 100]);
+saveas(gcf2, "./plots/nl_estimator_traj_3D.eps");
+tikzfilename = strcat(currentFolder,'/tikzfiles/nl_estimator_traj_3D.tex');
+cleanfigure; 
+matlab2tikz('filename',tikzfilename);
+
+%% Part II
+
+%% Part III
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+%.
+
