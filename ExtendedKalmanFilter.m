b554553f16d2c7a508b2c15e5eaf625befb16aaa
Alexander <alberndt@kth.se>
2020-09-25 21:02:22 +0200

    Updated plotting, and code readability

--- ExtendedKalmanFilter.m
+++ ExtendedKalmanFilter.m
@@ -28,74 +28,57 @@ function est=ExtendedKalmanFilter(gps_data,ref_data)
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-N = length(gps_data(1).PseudoRange);        % length of data 
-M = length(gps_data);                       % number of satellites (=30)
-est.x_h = zeros(7,N);                       % estimate of states
-est.P = zeros(7,7,N);                       % covariance matrix P 
+%% Initialization
 
-% get data from ref_data_struct
-s2r     = ref_data.s2r;
-PSD_clk = ref_data.PSD_clk;
-Ts = ref_data.Ts;
+N       =   length(gps_data(1).PseudoRange);	% length of data 
+M       =   length(gps_data);                   % number of satellites (=30)
+est.x_h =   zeros(8,N);                         % estimate of states
+est.P   =   zeros(8,8,N);                       % covariance matrix P 
+est.ek  =   NaN(M,N);                         % innovation
+s2r     =   ref_data.s2r;                       %
+PSD_clk =   ref_data.PSD_clk;                   % 
+Ts      =   ref_data.Ts;                        % sampling time
 
-F_x =   [1 Ts; 0 1];
-F_y =   [1 Ts; 0 1];
-F_z =   [1]; 
-F_clk = [1 Ts; 0 1]; 
+%% Construct process matrix F for model
 
-F = blkdiag(F_x,F_y,F_z,F_clk);
+F_x     =   [1 Ts; 0 1];
+F_y     =   [1 Ts; 0 1];
+F_z     =   [1 Ts; 0 1]; 
+F_clk   =   [1 Ts; 0 1]; 
+F       =   blkdiag(F_x,F_y,F_z,F_clk);
 
-G_x =   [Ts^2/2; Ts];
-G_y =   [Ts^2/2; Ts];
-G_z =   Ts;
+%% Construct process matrix F for model
 
-G   = blkdiag(G_x,G_y,G_z,eye(2));
+xhat_k_km1      = zeros(8,1); % estimate vector
 
-xhat_k_km1 = zeros(7,1); % estimate vector
-xhat_k_km1(6) = ref_data.x_clk(1);
-xhat_k_km1(7) = ref_data.x_clk(2);
+%% Determine process noise covariance Q
 
-P_k = Inf;
+S_x         = 1.0; 
+S_y         = 1.0; 
+S_z         = 0.8e-2; 
+Qtilde_2    = [[(Ts^4)/3 (Ts^3)/2];
+               [(Ts^3)/2 Ts^2]];
 
-S_phi = PSD_clk(1);
-S_f = PSD_clk(2);
-Q_clk = [[(S_phi*Ts + S_f*(Ts^3)/3) (Ts^2*S_f)];
-           [(Ts^2*S_f) (S_f*Ts)]];
-
-% MEASUREMENT COVARIANCE MATRIX
-R_k = s2r*eye(M);
-
-sigma2_x = 0.1;
-sigma2_y = sigma2_x;
-sigma2_z = sigma2_x;
-
-Q_x = sigma2_x;
-Q_y = sigma2_y;
-Q_z = sigma2_z;
-
-Q = blkdiag(Q_x,Q_y,Q_z,Q_clk);
-
-
-%% Model Equations 16-17
-
-S_x = 0.1e1; % TODO: update the 
-S_y = S_x;%1.0e-10;
-S_z = S_x;
-Qtilde_2 = [[(Ts^4)/3 (Ts^3)/2];
-            [(Ts^3)/2 Ts^2]];
-
-Q_x = S_x/Ts*Qtilde_2;
-Q_y = S_y/Ts*Qtilde_2;
-Q_z = S_z/Ts;
+Q_x         = S_x/Ts*Qtilde_2;
+Q_y         = S_y/Ts*Qtilde_2;
+Q_z         = S_z/Ts*Qtilde_2;
  
-S_phi = PSD_clk(1);
-S_f = PSD_clk(2);
-Q_clk = [[(S_phi*Ts + S_f*(Ts^3)/3) (Ts^2*S_f)];
-           [(Ts^2*S_f) (S_f*Ts)]];
+S_phi       = PSD_clk(1);
+S_f         = PSD_clk(2);
+Q_clk       = [[(S_phi*Ts + S_f*(Ts^3)/3) (Ts^2*S_f)];
+               [(Ts^2*S_f) (S_f*Ts)]];
 
-Q = blkdiag(Q_x,Q_y,Q_z,Q_clk);
+Q           = blkdiag(Q_x,Q_y,Q_z,Q_clk);
 
-%%
+%% Determine measurement noise covariance R
+
+R = s2r*eye(M);
+
+%% Initialize estimate covariance
+
+P_k         = zeros(8,8);
+
+%% Run the Simulation
 
 for n=1:N
     
@@ -103,7 +86,7 @@ for n=1:N
     y_i_tilde_vec   = zeros(M,1); 
     y_i_vec         = zeros(M,1);
     h_nonlinear     = zeros(M,1);
-    H               = zeros(M,7);
+    H               = zeros(M,8);
     
     satellite_avail = zeros(M,1);
     
@@ -111,70 +94,74 @@ for n=1:N
         % check if satellite measurement i is available (is NOT NAN)
         if ~isnan(gps_data(i).PseudoRange(n))
             
-            satellite_avail(i) = 1;
+            satellite_avail(i) = 1; %1 = true
 
             % position (x,y,z) of satellite m
-            p_i = gps_data(i).Satellite_Position_NED(:,n);
+            p_i     = gps_data(i).Satellite_Position_NED(:,n);
 
             % partial derivative elements of  h'(x)
-            h_p_1 = h_prime_func(p_i,xhat_k_km1,'x');
-            h_p_2 = 0;
-            h_p_3 = h_prime_func(p_i,xhat_k_km1,'y');
-            h_p_4 = 0;
-            h_p_5 = h_prime_func(p_i,xhat_k_km1,'z');
-            h_p_6 = ref_data.c;
-            h_p_7 = 0;
+            h_p_1   = h_prime_func(p_i,xhat_k_km1,'x');
+            h_p_2   = 0;
+            h_p_3   = h_prime_func(p_i,xhat_k_km1,'y');
+            h_p_4   = 0;
+            h_p_5   = h_prime_func(p_i,xhat_k_km1,'z');
+            h_p_6   = 0;
+            h_p_7   = ref_data.c;
+            h_p_8   = 0;
 
             % partial derivative vector h'(x) evaluated at xhat_k_km1
-            h_prime = [h_p_1 h_p_2 h_p_3 h_p_4 h_p_5 h_p_6 h_p_7];
+            h_prime     = [h_p_1 h_p_2 h_p_3 h_p_4 h_p_5 h_p_6 h_p_7 h_p_8];
 
             % measurement from satellite
-            y_i = gps_data(i).PseudoRange(n);
+            y_i         = gps_data(i).PseudoRange(n);
 
             % LHS of linearization y^i - h(xh) + h'(xh)xh     
             % where xh = \hat{x}
-            y_i_tilde = y_i - h_func(p_i, xhat_k_km1, ref_data) + h_prime*xhat_k_km1;
+            y_i_tilde           = y_i - h_func(p_i, xhat_k_km1, ref_data) + h_prime*xhat_k_km1;
             
-            h_nonlinear(i) = h_func(p_i, xhat_k_km1, ref_data);
+            h_nonlinear(i)      = h_func(p_i, xhat_k_km1, ref_data);
             
-            y_i_vec(i) = y_i;
-            y_i_tilde_vec(i) = y_i_tilde;
-            H(i,:) = h_prime;
+            y_i_vec(i)          = y_i;
+            y_i_tilde_vec(i)    = y_i_tilde;
+            H(i,:)              = h_prime;
         else
-            y_i_tilde_vec(i) = 0;
-            H(i,:)=zeros(1,7);
+            y_i_tilde_vec(i)    = 0;
+            H(i,:)              = zeros(1,8);
         end
     end
     
-    % LINEAR KF 
-    idxs = find(satellite_avail);
+    % determine idxs of available satellite measurements
+    idxs    =   find(satellite_avail); 
     
     H_sub               = H(idxs,:);
-    R_sub               = R_k(idxs,idxs);
+    R_sub               = R(idxs,idxs);
     y_i_vec_sub         = y_i_vec(idxs);
     h_nonlinear_sub     = h_nonlinear(idxs);
     
+    %% Linear Kalman Filter Equations 
+    
     % use the NL function for the innovation!
-    e_k = y_i_vec_sub - h_nonlinear_sub; 
+    e_k         =   y_i_vec_sub - h_nonlinear_sub; 
     
     if P_k == Inf
         disp("P_k is Inf - solving DARE");
         [P_k,~,~] = idare(F',H_sub',Q,R_sub,[],[]);
     end
     
-    R_ek = H_sub*P_k*H_sub' + R_sub;
-    K_k = F*P_k*H_sub'/R_ek;
+    R_ek        = H_sub*P_k*H_sub' + R_sub;
+    K_k         = F*P_k*H_sub'/R_ek;
     
-    P_kp1 = F*P_k*F' + Q - K_k*R_ek*K_k'; %G*Q_k*G'
-    xhat_kp1_k = F*xhat_k_km1 + K_k*e_k;
+    P_kp1       = F*P_k*F' + Q - K_k*R_ek*K_k';
+    xhat_kp1_k  = F*xhat_k_km1 + K_k*e_k;
+    
+    %% Set variables for next iteration
+    xhat_k_km1      = xhat_kp1_k;
+    P_k             = P_kp1;
 
-    % update time-index for next step
-    xhat_k_km1 = xhat_kp1_k;
-    P_k = P_kp1;
-
-    % Store the estimate
-    est.x_h(:,n) = xhat_kp1_k;
-    est.P(:,:,n) = P_kp1;
+    %%  Store the estimate
+    est.x_h(:,n)    = xhat_kp1_k;
+    est.e_k(idxs,n) = e_k;
+    est.P(:,:,n)    = P_kp1;
 
 end
 
--- main.m
+++ main.m
@@ -68,144 +68,182 @@
 
 clc
 clear
-close
+% close
 
 load("data/GPSdata.mat");
 currentFolder = pwd;
 
 %% Extended Kalman Filter
 
-s2r = ref_data_struct.s2r;
+% run the EKF
 est = ExtendedKalmanFilter(gps_data, ref_data_struct);
 
-x_h = est.x_h;
-P = est.P;
+%% Get estimated and true values for plotting
+
+% extract estimation states and covariances
+x_h         = est.x_h;
+P           = est.P;
+e_k         = est.e_k; 
 
 % obtain coordinate values 
-x           = x_h(1,:);
-y           = x_h(3,:);
-z           = x_h(5,:);
-delta_T     = x_h(6,:);
-delta_Tdot  = x_h(7,:);
+x            = x_h(1,:);
+y            = x_h(3,:);
+z            = x_h(5,:);
+delta_T      = x_h(7,:);
+delta_Tdot   = x_h(8,:);
 
 % true trajectory
-x_t = ref_data_struct.traj_ned(1,:);
-y_t = ref_data_struct.traj_ned(2,:);
-z_t = ref_data_struct.traj_ned(3,:);
+x_t          = ref_data_struct.traj_ned(1,:);
+y_t          = ref_data_struct.traj_ned(2,:);
+z_t          = ref_data_struct.traj_ned(3,:);
+delta_T_t    = ref_data_struct.x_clk(1,:);
+delta_Tdot_t = ref_data_struct.x_clk(2,:);
 
-% plot x-y 2D
+% time vector
+t   =   ref_data_struct.Ts.*(0:1:length(x));
+
+%% Plotting
+
+% plot errors between a range 480 to 2000
+t_min = 480;
+t_max = 2000;
+t_vec = t(t_min:t_max);
+
+gcf5 = figure(5);
+clf;
+
+subplot(5,1,1);
+stairs(t_vec,x(t_min:t_max));
+hold on
+grid on
+plot(t_vec,x_t(t_min:t_max));
+ylabel("x [m]");
+legend("EKF","true");
+
+subplot(5,1,2);
+stairs(t_vec,y(t_min:t_max));
+hold on
+grid on
+plot(t_vec,y_t(t_min:t_max));
+ylabel("y [m]");
+legend("EKF","true");
+
+subplot(5,1,3);
+stairs(t_vec,z(t_min:t_max));
+hold on
+grid on
+plot(t_vec,z_t(t_min:t_max));
+ylabel("z [m]");
+legend("EKF","true");
+
+subplot(5,1,4);
+stairs(t_vec,delta_T(t_min:t_max));
+hold on
+grid on
+plot(t_vec,delta_T_t(t_min:t_max));
+ylabel("$\Delta T$ [m]",'Interpreter','Latex');
+legend("EKF","true");
+
+subplot(5,1,5);
+stairs(t_vec,delta_Tdot(t_min:t_max));
+hold on
+grid on
+plot(t_vec,delta_Tdot_t(t_min:t_max));
+ylabel("$\dot{\Delta} T$ [m]",'Interpreter','Latex');
+xlabel("t(k)");
+legend("EKF","true");
+
+%% Plot the covariance matrix entries
+
+t_min = 1;
+t_max = 2000;
+t_vec = t(t_min:t_max);
+
+gcf6 = figure(6);
+clf;
+
+subplot(5,1,1);
+stairs(t_vec,reshape(est.P(1,1,t_min:t_max),[],1));
+grid on
+ylabel("P(1,1) - x");
+
+subplot(5,1,2);
+stairs(t_vec,reshape(est.P(3,3,t_min:t_max),[],1));
+grid on
+ylabel("P(3,3) - y");
+
+subplot(5,1,3);
+stairs(t_vec,reshape(est.P(5,5,t_min:t_max),[],1));
+grid on
+ylabel("P(5,5) - z");
+
+subplot(5,1,4);
+stairs(t_vec,reshape(est.P(7,7,t_min:t_max),[],1));
+grid on
+ylabel("P(7,7) - Delta T");
+
+subplot(5,1,5);
+stairs(t_vec,reshape(est.P(8,8,t_min:t_max),[],1));
+grid on
+xlabel("t(k)");
+ylabel("P(8,8) - Delta T dot");
+
+%% Innovation for specific satellite
+
+sat_number = 11;
+
+t_min = 400;
+t_max = 2000;
+t_vec = t(t_min:t_max);
+
+gcf7 = figure(7);
+clf;
+
+plot(t_vec, e_k(sat_number,t_min:t_max));
+grid on
+xlabel("t(k)");
+ylabel("innovation e_k(k)");
+
+%% 2D Plot
+
+t_min = 500;
+t_max = 800;
+
 gcf3 = figure(3);
 clf;
-plot(x,y);
+plot(x(t_min:t_max),y(t_min:t_max));
 grid on
 xlabel("x [m]");
 ylabel("y [m]");
 title("EKF - 2D");
 hold on
-plot(x_t,y_t,'r-.');
+plot(x_t(t_min:t_max),y_t(t_min:t_max),'r-.');
 legend("EKF","true");
 saveas(gcf3, "./plots/ekf_traj_2D.eps");
 tikzfilename = strcat(currentFolder,'/tikzfiles/ekf_traj_2D.tex');
 cleanfigure; 
 matlab2tikz('filename',tikzfilename);
 
-plot_extra = true;
+%% 3D Plot
 
-if plot_extra
+t_min = 500;
+t_max = 800;
 
-    % plot x-y-z 3D
-    gcf4 = figure(4);
-    clf;
-    plot3(x,y,z);
-    grid on
-    xlabel("x [m]");
-    ylabel("y [m]");
-    zlabel("z [m]");
-    title("EKF - 3D");
-    hold on
-    plot3(x_t,y_t,z_t,'r-.');
-    zlim([-100 100]);
-    saveas(gcf4, "./plots/ekf_traj_3D.eps");
-    tikzfilename = strcat(currentFolder,'/tikzfiles/ekf_traj_3D.tex');
-    cleanfigure; 
-    matlab2tikz('filename',tikzfilename);
-
-    % plot errors
-    t_range = 500; % plot only time-steps 0 to 500 
-
-    gcf5 = figure(5);
-    clf;
-
-    subplot(3,1,1);
-    stairs(x(1:t_range));
-    hold on
-    grid on
-    plot(x_t(1:t_range));
-    xlabel("k");
-    ylabel("x [m]");
-    legend("EKF","true");
-
-    subplot(3,1,2);
-    stairs(y(1:t_range));
-    hold on
-    grid on
-    plot(y_t(1:t_range));
-    xlabel("k");
-    ylabel("y [m]");
-    legend("EKF","true");
-
-    subplot(3,1,3);
-    stairs(z(1:t_range));
-    hold on
-    grid on
-    plot(z_t(1:t_range));
-    xlabel("k");
-    ylabel("z [m]");
-    legend("EKF","true");
-
-    %% Plot the covariance matrix entries
-
-    gcf6 = figure(6);
-    clf;
-
-    subplot(3,1,1);
-    stairs(reshape(est.P(1,1,1:t_range),[],1));
-    grid on
-    xlabel("k");
-    ylabel("P(1,1) - x");
-
-    subplot(3,1,2);
-    stairs(reshape(est.P(3,3,1:t_range),[],1));
-    grid on
-    xlabel("k");
-    ylabel("P(3,3) - y");
-
-    subplot(3,1,3);
-    stairs(reshape(est.P(5,5,1:t_range),[],1));
-    grid on
-    xlabel("k");
-    ylabel("P(5,5) - z");
-
-    % subplot(3,1,2);
-    % stairs(y(1:t_range));
-    % hold on
-    % grid on
-    % plot(y_t(1:t_range));
-    % xlabel("k");
-    % ylabel("y [m]");
-    % legend("EKF","true");
-    % 
-    % subplot(3,1,3);
-    % stairs(z(1:t_range));
-    % hold on
-    % grid on
-    % plot(z_t(1:t_range));
-    % xlabel("k");
-    % ylabel("z [m]");
-    % legend("EKF","true");
-end
-
-
+gcf4 = figure(4);
+clf;
+plot3(x(t_min:t_max),y(t_min:t_max),z(t_min:t_max));
+grid on
+xlabel("x [m]");
+ylabel("y [m]");
+zlabel("z [m]");
+title("EKF - 3D");
+hold on
+plot3(x_t(t_min:t_max),y_t(t_min:t_max),z_t(t_min:t_max),'r-.');
+zlim([-35 35]);
+saveas(gcf4, "./plots/ekf_traj_3D.eps");
+tikzfilename = strcat(currentFolder,'/tikzfiles/ekf_traj_3D.tex');
+cleanfigure; 
+matlab2tikz('filename',tikzfilename);
 
 %% Part III
 
